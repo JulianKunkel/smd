783c451e9267c0dfc347fa9a2ef9bb3886e14a32
Julian M. Kunkel <juliankunkel@googlemail.com>
2019-08-09 11:02:20 +0100

    Test

--- test/tst-int8-int16.c
+++ test/tst-int8-int16.c
@@ -15,25 +15,11 @@ int main() {
   int16_t *p1 = (int16_t *)out;
 
   int8_t ov = *(int8_t*)&i8;
-  // check the range
-  if(ov < SHRT_MIN || ov > SHRT_MAX){
-    return 1;
-  }
   *p1 = (int16_t) ov;
-
-  int16_t p2 = (int16_t) i8;
-
-  int16_t p3 = *(int16_t *)&i8;
 
   printf("\n\ni8 = %d", i8);
 
   printf("\n\np1 = %d", *p1);
-
-  printf("\n\np2 = %d", p2);
-
-  printf("\n\np3 = %d", p3);
-
-  printf("\n\n");
 
   return 0;
 }
